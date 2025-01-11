3ef010868f5615751d49caf983fe595023c990ea
SUFandom Xynoxx <bluepearl394@gmail.com>
2025-01-11 03:34:50 +0000

    a13

--- build_kernel.sh
+++ build_kernel.sh
@@ -19,7 +19,7 @@ build() {
 START_BUILD_TIME_RAW=$(TZ="Asia/Manila" date +%T)
 START_BUILD_DATE_RAW=$(TZ="Asia/Manila" date +%F)
 START_BUILD_TIME_AND_DATE="$START_BUILD_TIME_RAW - $START_BUILD_DATE_RAW"
-echo "Starting Compile of A12s Kernel"
+echo "Starting Compile of A13 Kernel"
 echo "Start Build: $START_BUILD_TIME_AND_DATE"
 
 git-init_
