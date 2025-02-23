eba4a0c3536f9625c4881a985ab8d7da8761b35b
SUFandom Xynoxx <bluepearl394@gmail.com>
2025-02-23 12:16:53 +0000

    feat: set printk to switches

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -2855,6 +2855,7 @@ static int buffers_allocated;
 
 void trace_printk_init_buffers(void)
 {
+	#ifdef CONFIG_TRACE_PRINTK
 	if (buffers_allocated)
 		return;
 
@@ -2867,13 +2868,10 @@ void trace_printk_init_buffers(void)
 	pr_warn("**********************************************************\n");
 	pr_warn("**   NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE   **\n");
 	pr_warn("**                                                      **\n");
-	pr_warn("** trace_printk() being used. Allocating extra memory.  **\n");
+	pr_warn("** Please tell Mizumo-prjkt to f-king disable this if   **\n");
+	pr_warn("** You aren't Mizumo-prjkt and tell him to get yourself **\n");
+	pr_warn("** A better kernel variant build                        **\n");
 	pr_warn("**                                                      **\n");
-	pr_warn("** This means that this is a DEBUG kernel and it is     **\n");
-	pr_warn("** unsafe for production use.                           **\n");
-	pr_warn("**                                                      **\n");
-	pr_warn("** If you see this message and you are not debugging    **\n");
-	pr_warn("** the kernel, report this immediately to your vendor!  **\n");
 	pr_warn("**                                                      **\n");
 	pr_warn("**   NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE   **\n");
 	pr_warn("**********************************************************\n");
@@ -2891,6 +2889,9 @@ void trace_printk_init_buffers(void)
 	 */
 	if (global_trace.trace_buffer.buffer)
 		tracing_start_cmdline_record();
+	#else
+		return;
+	#endif /* CONFIG_TRACE_PRINTK */
 }
 
 void trace_printk_start_comm(void)
